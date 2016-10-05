f082e0c2d124ee92cc9c5a9700bbb2b86980d25e
Slitzweitz <colinmbarlow@gmail.com>
2016-10-05 12:43:49 -0700

    Working solution to a synchronous I/O that counts number of new lines in a file

--- program.js
+++ program.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+//console.log("HELLO WORLD")
+// var sum = 0
+// var arrayCopy = process.argv
+// for (var i = 2; i < arrayCopy.length; i++) {
+// 	var arrayNumber = Number(arrayCopy[i])
+// 	sum += arrayNumber
+// 	}
+// console.log(sum)
+// var fs = require('fs');
+// var filePath = process.argv[2];
+// var data = fs.readFileSync(filePath, 'utf8');
+// var newLines = data.split(/\n/).length - 1;
+// console.log(newLines);
+// =======
 var sum = 0;
 var arrayCopy = process.argv;
 for (var i = 2; i <  arrayCopy.length; i++) {
@@ -6,3 +21,4 @@ for (var i = 2; i <  arrayCopy.length; i++) {
 	};
 console.log(sum)
 
+>>>>>>> f1ec0860550559ea0748335eae4567312173f078
