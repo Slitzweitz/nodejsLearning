65688e62c4e8b088c54fb5c36691bad81a0d135e
Slitzweitz <colinmbarlow@gmail.com>
2016-10-05 16:40:13 -0700

    changed comments to keep most recent task at bottom of file instead of commented out

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //console.log("HELLO WORLD")
 // var sum = 0
 // var arrayCopy = process.argv
@@ -7,18 +7,18 @@
 // 	sum += arrayNumber
 // 	}
 // console.log(sum)
-// var fs = require('fs');
-// var filePath = process.argv[2];
-// var data = fs.readFileSync(filePath, 'utf8');
-// var newLines = data.split(/\n/).length - 1;
-// console.log(newLines);
 // =======
-var sum = 0;
-var arrayCopy = process.argv;
-for (var i = 2; i <  arrayCopy.length; i++) {
-	var arrayNumber = Number(arrayCopy[i]);
-	sum += arrayNumber;
-	};
-console.log(sum)
+// var sum = 0;
+// var arrayCopy = process.argv;
+// for (var i = 2; i <  arrayCopy.length; i++) {
+// 	var arrayNumber = Number(arrayCopy[i]);
+// 	sum += arrayNumber;
+// 	};
+// console.log(sum)
 
->>>>>>> f1ec0860550559ea0748335eae4567312173f078
+var fs = require('fs');
+var filePath = process.argv[2];
+var data = fs.readFileSync(filePath, 'utf8');
+var newLines = data.split(/\n/).length - 1;
+console.log(newLines);
+//>>>>>>> f1ec0860550559ea0748335eae4567312173f078
