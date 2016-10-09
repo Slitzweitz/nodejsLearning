24799d48c9249600c8620305680efea07ef3401d
Slitzweitz <colinmbarlow@gmail.com>
2016-10-09 13:33:58 -0700

    Program to search a directory for certain file extensions succeessfully solves exercise 5, uses an asynchronous filesystem operation, readdir, to find all the files in a directory and filters them by extension

--- program.js
+++ program.js
@@ -21,13 +21,29 @@
 // console.log(newLines);
 
 //  Exercise 4: An Asynchronous filesystem operation!
+// var fs = require('fs');
+// function getData() {
+// 	fs.readFile(process.argv[2], 'utf8', function done(err, data) {
+// 		// console.log(data);
+// 	if (err) throw err;
+// 	var newLines = data.split(/\n/).length - 1;
+// 	console.log(newLines);
+// })
+// }
+// getData();
+
+//  Exercise 5: Filter a list of filenames in a directory for a certain file extension
 var fs = require('fs');
-function getData() {
-	fs.readFile(process.argv[2], 'utf8', function done(err, data) {
-		// console.log(data);
-	if (err) throw err;
-	var newLines = data.split(/\n/).length - 1;
-	console.log(newLines);
+function getFiles() {
+	fs.readdir(process.argv[2], function callback(err, list) {
+		if (err) throw err;
+		var extensionFilter = process.argv[3];
+		for (var i = 0; i < list.length; i++) {
+			var splitList = list[i].split(".");
+			if (splitList[1] == extensionFilter) {
+				console.log(list[i]);
+			}
+		}
 	})
 }
-getData();
+getFiles();
