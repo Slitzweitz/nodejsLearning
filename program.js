99d7b9a115e2e3c72cfe6ee39ca37bdc9e9e2fe8
Slitzweitz <colinmbarlow@gmail.com>
2016-10-11 22:44:16 -0700

    Solution to exercise 8, reads text from an http.get request

--- program.js
+++ program.js
@@ -1,36 +1,30 @@
-//  Exercise 1: of course, a hello world
-//console.log("HELLO WORLD")
+const http = require('http')
+const bl = require('bl')
 
-//  Exercise 2: Accessing the command line and arguments (passed to the command line)
-//  Adds elements together starting at position 2, through the end
-// var sum = 0
-// var arrayCopy = process.argv
-// for (var i = 2; i < arrayCopy.length; i++) {
-//  Must 'coerce' into a Number
-// 	var arrayNumber = Number(arrayCopy[i])
-// 	sum += arrayNumber
-// 	}
-// console.log(sum)
+http.get(process.argv[2], function callback(response) {
+	response.setEncoding("utf8")
+	response.pipe(bl(function (err, data) {
+		if (err) return err
+		console.log(data.length)
+		console.log(data.toString())
+	}))
+})
 
-//  Exercise 3: Synchronous filesystem operations!
-//  Require the filesystem module
-// var fs = require('fs');
-// var filePath = process.argv[2];
-// var data = fs.readFileSync(filePath, 'utf8');
-// var newLines = data.split(/\n/).length - 1;
-// console.log(newLines);
-
-//  Exercise 4: An Asynchronous filesystem operation!
-// var fs = require('fs');
-// function getData() {
-// 	fs.readFile(process.argv[2], 'utf8', function done(err, data) {
-// 		// console.log(data);
-// 	if (err) throw err;
-// 	var newLines = data.split(/\n/).length - 1;
-// 	console.log(newLines);
+//  Exercise 7: HTTP client. requires the http core and uses a http.get() method. URL is passed to get()
+// var http = require("http")
+//
+// http.get(process.argv[2], function callback (response) {
+// 	response.setEncoding("utf8")
+// 	response.on("data", function (data) {
+// 		console.log(data)
+// 	})
 // })
-// }
-// getData();
+
+//  Exercise 6: use a module to do the work, require it and call it passing in the command line arguments
+// var mymodule = require('./mymodule');
+// mymodule(process.argv[2],process.argv[3],function(err, data) {
+// 	if (err) return callback(err);
+// });
 
 //  Exercise 5: Filter a list of filenames in a directory for a certain file extension
 // var fs = require('fs');
@@ -48,18 +42,35 @@
 // }
 // getFiles();
 
-//  Exercise 6: use a module to do the work, require it and call it passing in the command line arguments
-// var mymodule = require('./mymodule');
-// mymodule(process.argv[2],process.argv[3],function(err, data) {
-// 	if (err) return callback(err);
-// });
+//  Exercise 4: An Asynchronous filesystem operation!
+// var fs = require('fs');
+// function getData() {
+// 	fs.readFile(process.argv[2], 'utf8', function done(err, data) {
+// 		// console.log(data);
+// 	if (err) throw err;
+// 	var newLines = data.split(/\n/).length - 1;
+// 	console.log(newLines);
+// })
+// }
+// getData();
 
-//  Exercise 7: HTTP client. requires the http core and uses a http.get() method. URL is passed to get()
-var http = require("http")
+//  Exercise 3: Synchronous filesystem operations!
+//  Require the filesystem module
+// var fs = require('fs');
+// var filePath = process.argv[2];
+// var data = fs.readFileSync(filePath, 'utf8');
+// var newLines = data.split(/\n/).length - 1;
+// console.log(newLines);
+//  Exercise 2: Accessing the command line and arguments (passed to the command line)
+//  Adds elements together starting at position 2, through the end
+// var sum = 0
+// var arrayCopy = process.argv
+// for (var i = 2; i < arrayCopy.length; i++) {
+//  Must 'coerce' into a Number
+// 	var arrayNumber = Number(arrayCopy[i])
+// 	sum += arrayNumber
+// 	}
+// console.log(sum)
 
-http.get(process.argv[2], function callback (response) {
-	response.setEncoding("utf8")
-	response.on("data", function (data) {
-		console.log(data)
-	})
-})
+//  Exercise 1: of course, a hello world
+//console.log("HELLO WORLD")
